9db25603d0b9bb66bd46b998064e11d4fbecc0ef
erinnelson13 <erin_nelson999@outlook.com>
2021-11-16 16:30:11 -0400

    Deleting unnecessary things

--- scrap script.R
+++ scrap script.R
@@ -26,34 +26,14 @@ trees_wf_bbox <- st_bbox(c(xmin = -63.5750000, ymin = 44.642000, xmax = -63.5660
                          crs = st_crs(background))
 background_cropped <- st_crop(background, trees_wf_bbox)
 
-#bg.sf <- st_sf(id = 'L1', st_sfc(st_linestring(as.matrix(background_cropped), dim = "XY")))
-#st_crs(background_cropped) <- st_crs(4326) # assign crs
-#bg.sf <- st_transform(background_cropped, crs = 32721) # transform
-
-#distances <- st_distance(x = trees.sf, y = bg.sf)
-#print(distances)
-#View(distances)
-
 trees_wf_bbox <- st_bbox(c(xmin = -63.5750000, ymin = 44.642000, xmax = -63.566000, ymax = 44.6510000),
                          crs = st_crs(background))
 background_cropped <- st_crop(background, trees_wf_bbox)
 
-#distances.filtered<- distances(!is.na(distances))
 
-
-
-
-
-  #load vector data
-  library(sf)
+#load vector data
 streets <- st_read("Street_Name_Routes.shp")
 trees <- st_read("Tree_Database.csv")
-
-#this calculates the minimum distance from your roads to one tree
-min(st_distance(background_cropped, trees.sf[1, ]))
-View(trees_wf_bbox)
-# but you need to repeat that over all the trees using sapply
-sapply(1:nrow(trees.sf), function(x) min(st_distance(background_cropped, trees.sf[x, ])))
 
 greg<-st_read("gregtrees.shp")
 tree_projected <- st_transform(trees.sf,st_crs(greg))
@@ -65,6 +45,7 @@ st_crs(streets_transform)
 #this calculates the minimum distance from your roads to one tree
 min(st_distance(streets_transform, tree_projected[1, ]))
 View(trees_wf_bbox)
+
 # but you need to repeat that over all the trees using sapply
 treedist1 <- sapply(1:nrow(tree_projected), function(x) min(st_distance(streets_transform, tree_projected[x, ])))
 wf_trees <- cbind(tree_projected, treedist1)
